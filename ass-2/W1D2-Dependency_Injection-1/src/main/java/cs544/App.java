bb4013583e1fb876dd72918b0e9af6c68a18e2b2
sayal-aryal <sayalaryal2@gmail.com>
2023-05-30 16:36:44 -0500

    Added: Assignment 1 Done

---
+++ ass-2/W1D2-Dependency_Injection-1/src/main/java/cs544/App.java
@@ -0,0 +1,14 @@
+package cs544;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+
+       ApplicationContext context = new ClassPathXmlApplicationContext("springconfig.xml");
+       Greeting greeting = context.getBean("greeting", Greeting.class);
+       greeting.sayHello();
+    } 
+}
