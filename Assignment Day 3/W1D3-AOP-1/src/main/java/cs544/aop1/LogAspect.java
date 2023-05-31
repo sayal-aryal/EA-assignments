fc6dc4153a3db52f2b703588eeafa5d1bf899f3d
sayal-aryal <sayalaryal2@gmail.com>
2023-05-31 16:58:50 -0500

    Added: save method execution time calculated added

--- Assignment Day 3/W1D3-AOP-1/src/main/java/cs544/aop1/LogAspect.java
+++ Assignment Day 3/W1D3-AOP-1/src/main/java/cs544/aop1/LogAspect.java
@@ -1,10 +1,13 @@
 package cs544.aop1;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -20,17 +23,31 @@ public class LogAspect {
 //        System.out.println(new Date() +" method= "+ joinPoint.getSignature().getName());
 //    }
 
-    @Pointcut("execution(* cs544.aop1.EmailSender.sendEmail(String, String))")
+    @Pointcut("execution(* cs544.aop1.EmailSender.sendEmail(*, *))")
     public void logEmailSend() {
+    }
+
+    @Pointcut("execution(* cs544.aop1.CustomerDAO.save(*))")
+    public void logDAOsave() {
     }
 
     @After("logEmailSend()")
     public void afterSendEmail(JoinPoint joinPoint) {
-        System.out.println(new Date() +" method= "+ joinPoint.getSignature().getName());
+        System.out.println(new Date() + " method= " + joinPoint.getSignature().getName());
         Object[] objs = joinPoint.getArgs();
-        System.out.println("address = "+objs[0]+" message = "+objs[1]);
+        System.out.println("address = " + objs[0] + " message = " + objs[1]);
         EmailSender sender = (EmailSender) joinPoint.getTarget();
         System.out.println("outgoing mail server = " + sender.outgoingMailServer);
+    }
+
+    @Around("logDAOsave()")
+    public void logTimeToSave(ProceedingJoinPoint call) throws Throwable {
+        StopWatch sw = new StopWatch();
+        sw.start(call.getSignature().getName());
+        Object retVal = call.proceed();
+        sw.stop();
+        long totaltime = sw.getLastTaskTimeMillis();
+        System.out.println("The total time to perform the save operation is " + totaltime + " ms");
     }
 
 
